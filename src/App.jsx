1e91ec4e7337994b7afaade4d4ecbe8c6e89f0fd
lpc634 <lpc634@gmail.com>
2025-07-18 17:10:32 +0100

    Fix: Forcing update to App.jsx import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth, AuthProvider } from './useAuth';
-import LoginPage from './pages/LoginPage.jsx';
+import LoginPage from './pages/LoginPage.jsx'; // Forcing Git to see this change
 import Layout from './Layout';
 import { Toaster as SonnerToaster } from "./components/ui/Toaster";
 import Dashboard from './pages/Dashboard';
