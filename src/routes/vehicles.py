6fe24c5ffc05d4031b8e09d44b8b7f688761e5ab
lpc634 <lpc634@gmail.com>
2025-07-23 16:24:15 +0100

    Fix vehicle search and add map display

--- src/routes/vehicles.py
+++ src/routes/vehicles.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from src.models.user import User
-from src.models.vehicle import VehicleSighting # --- CHANGED: Imports from the new vehicle.py file
+from src.models.vehicle import VehicleSighting 
 from src.extensions import db
 from sqlalchemy import desc
 
