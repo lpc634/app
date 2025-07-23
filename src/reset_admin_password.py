4b5260f862fe379aaafbebe79c947e64cc9434ae
lpc634 <lpc634@gmail.com>
2025-07-23 17:15:10 +0100

    Correct admin email in reset script

--- src/reset_admin_password.py
+++ src/reset_admin_password.py
@@ -4,13 +4,13 @@ from main import app, db
 from src.models.user import User
 import os
 
-# --- IMPORTANT: Change these values ---
-ADMIN_EMAIL = "admin@v3services.com"
+# --- THIS IS THE ONLY CHANGE ---
+ADMIN_EMAIL = "info@v3-services.com"
 NEW_PASSWORD = "admin123!"  # <-- CHOOSE YOUR NEW PASSWORD HERE
 
 with app.app_context():
     admin_user = User.query.filter_by(email=ADMIN_EMAIL).first()
-    
+
     if admin_user:
         admin_user.set_password(NEW_PASSWORD)
         db.session.commit()
